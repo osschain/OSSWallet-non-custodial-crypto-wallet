b04caf546195d546868044d9769acdc6798f2bcb
Amirani <amiraniburjanadze@gmail.com>
2024-04-23 20:00:33 +0400

    feat: create BodyText Ui component

---
+++ src/components/ui/BodyTextUi.tsx
@@ -0,0 +1,28 @@
+import { Text } from "react-native";
+import { DefaultTheme, styled } from "styled-components/native";
+
+type Sizes = "lg" | "md" | "sm";
+type Weights = "medium" | "regular";
+
+interface PropsType {
+  size: Sizes;
+  theme: DefaultTheme;
+  weight: Weights;
+}
+
+export const BodyTextUi = styled(Text)<PropsType>`
+  ${({ size, theme, weight }) => `
+      font-family:  ${theme.fonts["body"]};
+      font-size: ${theme.fontSizes[`body-${size}`]}; 
+      font-weight: ${theme.fontWeights[weight]};
+      line-height: ${theme.lineHeights[`${size}`]};
+      color: ${theme.colors["text-primary"]};
+    `}
+`;
+
+BodyTextUi.defaultProps = {
+  size: "md",
+  weight: "regular",
+};
+
+export default BodyTextUi;

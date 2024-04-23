2dce4ea9bad1179233a2838384d93a8b4237b80c
Amirani <amiraniburjanadze@gmail.com>
2024-04-23 16:54:03 +0400

    update: use import aliases in theme index file

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,9 +1,9 @@
 import { DefaultTheme } from "styled-components/native";
 
-import { colorsLight, colorsDark } from "./colors";
-import { fonts, fontWeights, fontSizes } from "./fonts";
-import { sizes } from "./sizes";
-import { spaces, lineHeights } from "./spacing";
+import { colorsLight, colorsDark } from "@/themes/colors";
+import { fonts, fontWeights, fontSizes } from "@/themes/fonts";
+import { sizes } from "@/themes/sizes";
+import { spaces, lineHeights } from "@/themes/spacing";
 
 export const lightTheme: DefaultTheme = {
   colors: colorsLight,

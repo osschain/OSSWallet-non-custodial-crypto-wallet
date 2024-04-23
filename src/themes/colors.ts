f074ba9d06155f50a07acf9cdb0fe6e223444e45
Amirani <amiraniburjanadze@gmail.com>
2024-04-23 16:14:34 +0400

    feat: add colors at theming colors

--- src/themes/colors.ts
+++ src/themes/colors.ts
@@ -1,2 +1,41 @@
-export const colorsLight = { primary: "red" };
-export const colorsDark = { primary: "green" };
+export const colorsLight = {
+  "yellow-500": "#F4AD42",
+  "text-primary": "#141A21",
+  "text-second": "#5A6876",
+  "bg-primary": "#FFFFFF",
+  "bg-second": "#EFF4FC",
+  "bg-third": "#F3F9FE",
+  "border-color": "#E8EFFB",
+  "bg-input": "#CCCCCC",
+  "light-100": "#F7FBFE",
+  "blue-700": "#005AEF",
+  "blue-500": "#1A70FE",
+  "blue-100": "#0066FF",
+  "green-700": "#20B56D",
+  "green-500": "#2CCD80",
+  "green-100": "#27AE60",
+  "red-700": "#DB3F3F",
+  "red-500": "#EB5757",
+  "red-100": "#F44242",
+};
+
+export const colorsDark = {
+  "yellow-500": "#F4AD42",
+  "text-primary": "#FFFFFF",
+  "text-second": "#BFC3C9",
+  "bg-primary": "#141A21",
+  "bg-second": "#1C232C",
+  "bg-third": "#212931",
+  "border-color": "#39404F",
+  "bg-input": "#222222",
+  "light-100": "#0D1117",
+  "blue-700": "#00225A",
+  "blue-500": "#0040A6",
+  "blue-100": "#005AEF",
+  "green-700": "#14753F",
+  "green-500": "#1E8C4E",
+  "green-100": "#20B56D",
+  "red-700": "#992929",
+  "red-500": "#B03232",
+  "red-100": "#DB3F3F",
+};

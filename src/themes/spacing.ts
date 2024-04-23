80bfff39a00cc692de5bb789bc6bda4c33cb333b
Amirani <amiraniburjanadze@gmail.com>
2024-04-23 16:50:57 +0400

    feat: add spacing

--- src/themes/spacing.ts
+++ src/themes/spacing.ts
@@ -1,3 +1,15 @@
-export const spaces = {};
+// prettier-ignore
+export const lineHeights = {
+  "title": "28px",
+  "copy": "20px",
+};
 
-export const lineHeights = {};
+// prettier-ignore
+export const spaces = {
+  "sm": "2px",
+  "md": "4px",
+  "lg": "8px",
+  "xl": "16px",
+  "2xl": "32px",
+  "3xl": "64px",
+};

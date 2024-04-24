73e37c39900dd142c330913149d6e9551400819c
Amirani <amiraniburjanadze@gmail.com>
2024-04-24 21:08:46 +0400

    feat: import fonts and create hook for import them

---
+++ src/hooks/useFonts.tsx
@@ -0,0 +1,15 @@
+import { useFonts } from "expo-font";
+
+export default function useFont() {
+  const [fontsLoaded, fontError] = useFonts({
+    "Inter-thin": require("@/assets/fonts/Inter-Thin.ttf"),
+    "Inter-400": require("@/assets/fonts/Inter-Regular.ttf"),
+    "Inter-500": require("@/assets/fonts/Inter-Medium.ttf"),
+    "Inter-light": require("@/assets/fonts/Inter-Light.ttf"),
+    "Inter-600": require("@/assets/fonts/Inter-Semi.ttf"),
+    "Inter-extraBold": require("@/assets/fonts/Inter-ExtraBold.ttf"),
+    "Inter-700": require("@/assets/fonts/Inter-Bold.ttf"),
+  });
+
+  return { fontsLoaded, fontError };
+}

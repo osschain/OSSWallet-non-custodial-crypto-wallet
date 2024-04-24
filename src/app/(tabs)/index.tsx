ac7caaec0c6c822a5109a506a8d3e06cf82cd4a3
Amirani <amiraniburjanadze@gmail.com>
2024-04-24 21:39:38 +0400

    feat: implement segment control component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,19 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
-import styled from "styled-components/native";
+import DefaultHeader from "@/components/layout/DefaultHeader";
+import SecondHeader from "@/components/layout/SecondHeader";
+import { SegmentedControl } from "@/components/segment";
+import ButtonUi from "@/components/ui/ButtonUi";
+import CheckBoxUi from "@/components/ui/CheckBoxUi";
+import SpacerUi from "@/components/ui/SpacerUi";
+import { SwitchContainerUi, SwitchUi } from "@/components/ui/SwitchUi";
+import WalletCard from "@/components/wallet/wallet-card";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { useStyledTheme } from "@/providers/StyledThemeProvider";
-
-const Title = styled(Text)`
-  font-size: 16px;
-  color: ${({ theme }) => theme.colors["yellow-500"]};
-`;
+const options = ["Assets", "NFT", "History"];
 
 export default function TabOneScreen() {
-  const { toggleTheme } = useStyledTheme();
+  const [selectedOption, setSelectedOption] = useState("Assets");
 
   return (
     <View>
-      <Title onPress={toggleTheme}>HI Felas</Title>
+      <SegmentedControl
+        options={options}
+        selectedOption={selectedOption}
+        onOptionPress={setSelectedOption}
+      />
+      <SpacerUi size="xl">
+        <WalletCard />
+      </SpacerUi>
+      <SpacerUi>
+        <CheckBoxUi />
+      </SpacerUi>
+      <SpacerUi>
+        <SwitchContainerUi>
+          <SwitchUi />
+        </SwitchContainerUi>
+      </SpacerUi>
+      <SpacerUi>
+        <ButtonUi>Button</ButtonUi>
+      </SpacerUi>
+      <SpacerUi>
+        <DefaultHeader />
+      </SpacerUi>
+      <SpacerUi>
+        <SecondHeader />
+      </SpacerUi>
     </View>
   );
 }
---
+++ src/components/segment/index.tsx
@@ -0,0 +1,127 @@
+import React from "react";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  useWindowDimensions,
+} from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
+import styled from "styled-components/native";
+
+import BodyTextUi from "@/components/ui/BodyTextUi";
+
+const Container = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 55px;
+  border-bottom-width: 0.2px;
+  border-color: ${($props) => $props.theme.colors["blue-500"]};
+`;
+
+const SegmentLabel = styled(BodyTextUi)<{ isActive: boolean }>`
+  color: ${(props) =>
+    props.isActive
+      ? props.theme.colors["blue-500"]
+      : props.theme.colors["text-second"]};
+`;
+
+const LabelContainer = styled(TouchableOpacity)`
+  justify-content: center;
+  align-items: center;
+`;
+
+const ActiveBox = styled(Animated.View)`
+  position: absolute;
+  border-radius: 10px;
+  height: 4px;
+  bottom: -2px;
+  background-color: blue;
+`;
+
+type Props = {
+  options: string[];
+  selectedOption: string;
+  onOptionPress?: (option: string) => void;
+};
+
+const SegmentedControl: React.FC<Props> = React.memo(
+  ({ options, selectedOption, onOptionPress }) => {
+    const { width: windowWidth } = useWindowDimensions();
+
+    const internalPadding = 20;
+    const segmentedControlWidth = windowWidth;
+
+    const itemWidth =
+      (segmentedControlWidth - internalPadding) / options.length;
+
+    const rStyle = useAnimatedStyle(() => {
+      return {
+        left: withTiming(
+          itemWidth * options.indexOf(selectedOption) +
+            internalPadding / 2 +
+            itemWidth / 4
+        ),
+      };
+    }, [selectedOption, options, itemWidth]);
+
+    return (
+      <Container
+        style={[
+          {
+            width: segmentedControlWidth,
+            paddingLeft: internalPadding / 2,
+          },
+        ]}
+      >
+        <ActiveBox
+          style={[
+            {
+              width: itemWidth / 2,
+              backgroundColor: "red",
+            },
+            rStyle,
+            styles.activeBox,
+          ]}
+        />
+        {options.map((option) => {
+          return (
+            <LabelContainer
+              onPress={() => {
+                onOptionPress?.(option);
+              }}
+              key={option}
+              style={[
+                {
+                  width: itemWidth,
+                },
+              ]}
+            >
+              <SegmentLabel
+                isActive={option === selectedOption}
+                size="lg"
+                weight="medium"
+              >
+                {option}
+              </SegmentLabel>
+            </LabelContainer>
+          );
+        })}
+      </Container>
+    );
+  }
+);
+
+const styles = StyleSheet.create({
+  activeBox: {
+    position: "absolute",
+    borderRadius: 10,
+    height: 4,
+    bottom: -2,
+    backgroundColor: "blue",
+  },
+});
+
+export { SegmentedControl };

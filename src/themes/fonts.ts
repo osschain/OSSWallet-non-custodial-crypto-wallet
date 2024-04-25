991c2419f9a041df8fbfc26eca04cf6964cf9d6a
Amirani <amiraniburjanadze@gmail.com>
2024-04-25 04:43:39 +0400

    update: update fontSizes theme architecture

--- src/themes/fonts.ts
+++ src/themes/fonts.ts
@@ -1,33 +1,34 @@
-// prettier-ignore
 export const fonts = {
-  "body": "Inter",
-  "heading": "Inter",
+  body: "Inter",
+  heading: "Inter",
 };
 
-// prettier-ignore
 export const fontWeights = {
-  'bold': 700,
-  "semi": 600,
-  "regular": 400,
-  "medium": 500,
+  bold: 700,
+  semi: 600,
+  regular: 400,
+  medium: 500,
 };
 
-// prettier-ignore
 export const lineHeights = {
-    "2xl": "32px",
-    "xl": "28px",
-    "lg": "26px",
-    "md": "24px",
-    "sm": "22px",
-  };
+  "2xl": "32px",
+  xl: "28px",
+  lg: "26px",
+  md: "24px",
+  sm: "22px",
+};
 
 export const fontSizes = {
-  "header-2xl": "24px",
-  "header-xl": "20px",
-  "header-lg": "18px",
-  "header-md": "16px",
-  "header-sm": "14px",
-  "body-lg": "16px",
-  "body-md": "14px",
-  "body-sm": "12px",
+  header: {
+    "2xl": "24px",
+    xl: "20px",
+    lg: "18px",
+    md: "16px",
+    sm: "14px",
+  },
+  body: {
+    lg: "16px",
+    md: "14px",
+    sm: "12px",
+  },
 };

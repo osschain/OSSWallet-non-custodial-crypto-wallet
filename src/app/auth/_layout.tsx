ccf1c6a193429903d2b066065d4113a0933505a9
Amirani <amiraniburjanadze@gmail.com>
2024-04-25 19:23:51 +0400

    feat: set background color at auth screen

--- src/app/auth/_layout.tsx
+++ src/app/auth/_layout.tsx
@@ -1,15 +1,20 @@
 import { Redirect, Stack } from "expo-router";
+import { useTheme } from "styled-components/native";
 
 import { useAuth } from "@/providers/AuthProvider";
 
 export default function AuthLayout() {
   const { masterKey } = useAuth();
-
+  const theme = useTheme();
   if (masterKey) {
     return <Redirect href="/" />;
   }
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        contentStyle: { backgroundColor: theme.colors["bg-primary"] },
+      }}
+    >
       <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );

7f699dc10ed6daa231bbbcedffd7eff223a72b42
Amirani <amiraniburjanadze@gmail.com>
2024-04-25 04:41:50 +0400

    update: create seperate interfaces for theme types and export it keys types

--- styled.d.ts
+++ styled.d.ts
@@ -1,77 +1,104 @@
 import "styled-components/native";
 
+interface Colors {
+  "yellow-500": string;
+  "text-primary": string;
+  "text-second": string;
+  "bg-primary": string;
+  "bg-second": string;
+  "bg-third": string;
+  "border-color": string;
+  "bg-input": string;
+  "light-100": string;
+  "blue-700": string;
+  "blue-500": string;
+  "blue-100": string;
+  "green-700": string;
+  "green-500": string;
+  "green-100": string;
+  "red-700": string;
+  "red-500": string;
+  "red-100": string;
+  "pure-white": string;
+}
+
+interface Spacing {
+  sm: string;
+  md: string;
+  lg: string;
+  xl: string;
+  "2xl": string;
+  "3xl": string;
+  "4xl": string;
+}
+
+interface Sizes {
+  sm: string;
+  md: string;
+  lg: string;
+  xl: string;
+  "2xl": string;
+  "3xl": string;
+  "4xl": string;
+}
+
+interface HeaderFontSizes {
+  "2xl": string;
+  xl: string;
+  lg: string;
+  md: string;
+  sm: string;
+}
+
+interface BodyFontSizes {
+  lg: string;
+  md: string;
+  sm: string;
+}
+
+interface FontSizes {
+  header: HeaderFontSizes;
+  body: BodyFontSizes;
+}
+
+interface FontWeights {
+  bold: number;
+  semi: number;
+  regular: number;
+  medium: number;
+}
+
+interface Fonts {
+  body: string;
+  heading: string;
+}
+
+interface LineHeights {
+  "2xl": string;
+  xl: string;
+  lg: string;
+  md: string;
+  sm: string;
+}
+
+export type ColorsType = keyof Colors;
+export type SpacingType = keyof Spacing;
+export type SizesType = keyof Sizes;
+export type FontSizesType = keyof FontSizes;
+export type FontWeightsType = keyof FontWeights;
+export type FontsType = keyof Fonts;
+export type LineHeighsType = keyof LineHeights;
+export type HeaderFontSizesType = keyof HeaderFontSizes;
+export type BodyFontSizesType = keyof BodyFontSizes;
+
 declare module "styled-components/native" {
   export interface DefaultTheme {
-    colors: {
-      "yellow-500": string;
-      "text-primary": string;
-      "text-second": string;
-      "bg-primary": string;
-      "bg-second": string;
-      "bg-third": string;
-      "border-color": string;
-      "bg-input": string;
-      "light-100": string;
-      "blue-700": string;
-      "blue-500": string;
-      "blue-100": string;
-      "green-700": string;
-      "green-500": string;
-      "green-100": string;
-      "red-700": string;
-      "red-500": string;
-      "red-100": string;
-      "pure-white": string;
-    };
-    // prettier-ignore
-    spaces: {
-      "sm": string;
-      "md": string;
-      "lg": string;
-      "xl": string;
-      "2xl": string;
-      "3xl": string;
-      "4xl": string;
-    };
-    // prettier-ignore
-    sizes: {
-      "sm": string;
-      "md": string;
-      "lg": string;
-      "xl": string;
-      "2xl": string;
-      "3xl": string;
-      "4xl": string;
-    };
-    fontSizes: {
-      "header-2xl": string;
-      "header-xl": string;
-      "header-lg": string;
-      "header-md": string;
-      "header-sm": string;
-      "body-lg": string;
-      "body-md": string;
-      "body-sm": string;
-    };
-    // prettier-ignore
-    fontWeights: {
-      'bold': number;
-      "semi": number;
-      "regular": number;
-      "medium": number;
-    };
-    // prettier-ignore
-    fonts: {
-        "body": string,
-        "heading": string,
-    },
-    // prettier-ignore
-    lineHeights: {
-        "2xl": string,
-        "xl": string;
-        "lg": string;
-        "md": string;
-        "sm": string;
-    }
+    colors: Colors;
+    spaces: Spacing;
+    sizes: Sizes;
+    fontSizes: FontSizes;
+    fontWeights: FontWeights;
+    fonts: Fonts;
+    lineHeights: LineHeights;
   }
 }

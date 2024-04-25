d099150ac4d7bd5bc75d4e8adf479365b8fc4510
Amirani <amiraniburjanadze@gmail.com>
2024-04-25 04:59:30 +0400

    feat: add color prop at body and tetxt ui component

--- src/components/ui/BodyTextUi.tsx
+++ src/components/ui/BodyTextUi.tsx
@@ -1,22 +1,27 @@
 import { Text } from "react-native";
 import { DefaultTheme, styled } from "styled-components/native";
 
-import { BodyFontSizesType, FontWeightsType } from "../../../styled";
+import {
+  BodyFontSizesType,
+  ColorsType,
+  FontWeightsType,
+} from "../../../styled";
 
 import { getFontStyle } from "@/util/themeUtils";
 
 interface Props {
   size?: BodyFontSizesType;
+  color?: ColorsType;
   theme: DefaultTheme;
   weight?: FontWeightsType;
 }
 
 export const BodyTextUi = styled(Text)<Props>`
-  ${({ size = "md", theme, weight = "regular" }) => `
+  ${({ color = "text-primary", size = "md", theme, weight = "regular" }) => `
       font-family: ${getFontStyle(theme, "heading", weight)};;
       font-size: ${theme.fontSizes.body[size]}; 
       line-height: ${theme.lineHeights[`${size}`]};
-      color: ${theme.colors["text-primary"]};
+      color: ${theme.colors[color]};
     `}
 `;
 
--- src/components/ui/HeaderTextUi.tsx
+++ src/components/ui/HeaderTextUi.tsx
@@ -1,20 +1,26 @@
 import { Text } from "react-native";
 import { styled, DefaultTheme } from "styled-components/native";
 
-import { HeaderFontSizesType } from "@/theme/types";
+import {
+  ColorsType,
+  FontWeightsType,
+  HeaderFontSizesType,
+} from "@/theme/types";
 import { getFontStyle } from "@/util/themeUtils";
 
 interface Props {
   size?: HeaderFontSizesType;
+  weight: FontWeightsType;
   theme: DefaultTheme;
+  color: ColorsType;
 }
 
 export const HeaderTextUi = styled(Text)<Props>`
-  ${({ size = "md", theme }) => `
-    font-family: ${getFontStyle(theme, "heading", "semi")};;
+  ${({ size = "md", color = "text-primary", weight = "semi", theme }) => `
+    font-family: ${getFontStyle(theme, "heading", weight)};
     font-size: ${theme.fontSizes.header[size]}; 
     line-height: ${theme.lineHeights[`${size}`]};
-    color: ${theme.colors["text-primary"]};
+    color: ${theme.colors[color]};
   `}
 `;
 

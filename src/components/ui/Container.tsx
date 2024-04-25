bcd1dbcea5d7b0481a805edc4161e9423522d52f
Amirani <amiraniburjanadze@gmail.com>
2024-04-25 18:22:04 +0400

    fix: flex prop error in container component

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View<{ height: "full" | "auto" }>`
+export const Container = styled.View<{ height?: "full" | "auto" }>`
   padding: 0 ${({ theme }) => theme.spaces["xl"]};
-  flex: 0 ${({ height = "auto" }) => (height === "full" ? 1 : null)};
+  flex: ${({ height = "auto" }) => (height === "full" ? 1 : null)};
 `;

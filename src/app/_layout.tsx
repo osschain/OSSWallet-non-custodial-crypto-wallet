e59b83999b4b0b208b85fe7f034c105ea981669e
Amirani <amiraniburjanadze@gmail.com>
2024-04-25 19:23:26 +0400

    feat: remove header from auth screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -50,6 +50,7 @@ function RootLayoutNav() {
           <StyledThemeProvider>
             <Stack>
               <Stack.Screen name="index" options={{ headerShown: false }} />
+              <Stack.Screen name="auth" options={{ headerShown: false }} />
               <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
               <Stack.Screen name="modal" options={{ presentation: "modal" }} />
             </Stack>

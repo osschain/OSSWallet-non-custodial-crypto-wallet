1054c8140f32281e09fe4baebd63963271f19cc0
Amirani <amiraniburjanadze@gmail.com>
2024-05-02 23:00:53 +0400

    feat: redirect at enter password if there is encrypted seed but not password

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,18 @@
 import { Redirect } from "expo-router";
-import React, { useEffect } from "react";
+import React from "react";
 import { ActivityIndicator } from "react-native";
 
 import { useAuth } from "@/providers/AuthProvider";
 
 function Index() {
-  const { seed, loading } = useAuth();
+  const { seed, loading, password, encryptedSeed } = useAuth();
 
   if (loading) {
     return <ActivityIndicator style={{ marginTop: 64 }} />;
+  }
+
+  if (encryptedSeed && !password) {
+    return <Redirect href="/auth/enter-password" />;
   }
 
   if (!seed) {

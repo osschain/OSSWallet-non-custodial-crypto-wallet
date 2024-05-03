a3fd7e04a9e673e7f0c77a3ff3c425458b2d2eee
Amirani <amiraniburjanadze@gmail.com>
2024-05-03 18:43:24 +0400

    feat: create camera ui

---
+++ src/components/ui/CameraUi.tsx
@@ -0,0 +1,60 @@
+import { CameraView, useCameraPermissions } from "expo-camera/next";
+import { ComponentPropsWithRef } from "react";
+import { StyleSheet, View } from "react-native";
+
+import ButtonUi from "@/components/ui/ButtonUi";
+import HeaderTextUi from "@/components/ui/HeaderTextUi";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  padding: ${({ theme }) => theme.spaces["2xl"]};
+`;
+
+const CameraContainer = styled.View`
+  flex: 1;
+`;
+
+const Camera = styled(CameraView)`
+  flex: 1;
+`;
+
+const StyledHeaderTextUi = styled(HeaderTextUi)`
+  text-align: center;
+`;
+
+const Button = styled(ButtonUi)`
+  margin-top: ${({ theme }) => theme.spaces["2xl"]};
+`;
+
+export default function CameraUi({
+  ...rest
+}: ComponentPropsWithRef<typeof CameraView>) {
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <Container>
+        <StyledHeaderTextUi size="xl" style={{ textAlign: "center" }}>
+          We need your permission to show the camera
+        </StyledHeaderTextUi>
+        <Button variant="primary" onPress={requestPermission}>
+          Grant Permission{" "}
+        </Button>
+      </Container>
+    );
+  }
+
+  return (
+    <CameraContainer>
+      <Camera {...rest} />
+    </CameraContainer>
+  );
+}

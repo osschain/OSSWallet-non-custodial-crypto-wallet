8fd4c30fe2fffba5481428451e078a5eff4b5f30
Amirani <amiraniburjanadze@gmail.com>
2024-05-09 17:37:42 +0400

    feat: NetworkButton component

---
+++ src/components/network/NetworkButton.tsx
@@ -0,0 +1,45 @@
+import { EvilIcons } from "@expo/vector-icons";
+import { ComponentPropsWithoutRef, ReactNode } from "react";
+import { TouchableOpacity } from "react-native";
+import styled, { useTheme } from "styled-components/native";
+
+import BodyTextUi from "../ui/BodyTextUi";
+import ButtonUi from "../ui/ButtonUi";
+
+import { pixelToNumber } from "@/util/pixelToNumber";
+
+function NetworkButton({
+  children,
+  ...rest
+}: { children: ReactNode } & ComponentPropsWithoutRef<
+  typeof TouchableOpacity
+>) {
+  const theme = useTheme();
+
+  return (
+    <Button variant="secondary" {...rest}>
+      <FlexContainer style={{ flexDirection: "row", gap: 10 }}>
+        <BodyTextUi weight="medium">{children}</BodyTextUi>
+        <EvilIcons
+          name="arrow-up"
+          size={pixelToNumber(theme.sizes["xl"])}
+          color={theme.colors["text-primary"]}
+        />
+      </FlexContainer>
+    </Button>
+  );
+}
+
+const Button = styled(ButtonUi)`
+  width: 40%;
+  padding: ${({ theme }) => theme.spaces["lg"]}
+    ${({ theme }) => theme.spaces["lg"]};
+`;
+
+const FlexContainer = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  gap: ${({ theme }) => theme.spaces["md"]};
+`;
+
+export default NetworkButton;

aafacb234848b8b0662a286b9952faa9f6c03069
Amirani <amiraniburjanadze@gmail.com>
2024-05-09 17:38:13 +0400

    feat: add chains  and networks mock

--- src/util/mock.tsx
+++ src/util/mock.tsx
@@ -1,2 +1,32 @@
+import { defaultImage } from "./DefaultImage";
+
 export const mockedSeed =
   "apple banana carrot dog elephant fountain guitar helicopter iguana jellyfish kangaroo laptop";
+
+export const networks = [
+  {
+    id: 1,
+    image: defaultImage,
+    label: "Acala",
+  },
+  {
+    id: 2,
+    image: defaultImage,
+    label: "bitcoin",
+  },
+];
+
+export const chains = [
+  {
+    id: 1,
+    image: defaultImage,
+    title: "oss",
+    decription: "osschain",
+  },
+  {
+    id: 2,
+    image: defaultImage,
+    title: "btc",
+    decription: "bitcoin",
+  },
+];

1e37f6da9753f9bf8f2adfaaa43ea39c47af2f09
Amirani <amiraniburjanadze@gmail.com>
2024-05-18 12:42:37 +0400

    update: use updated createAsset function eth wallet ccreating sccreen

--- src/app/auth/wallet-creating.tsx
+++ src/app/auth/wallet-creating.tsx
@@ -2,25 +2,25 @@ import { router } from "expo-router";
 import { useEffect } from "react";
 
 import AuthLoading from "@/components/auth/AuthLoading";
+import { useAsset } from "@/providers/AssetProvider";
 import { useAuth } from "@/providers/AuthProvider";
-import { useWallet } from "@/providers/WalletProvider";
-import { createWallets } from "@/util/ethers";
+import { createAssets } from "@/util/ethers";
 
 export default function WalletCreating() {
-  const { addWallets } = useWallet();
+  const { addAssets } = useAsset();
   const { seed, setupPass } = useAuth();
 
   useEffect(() => {
     setTimeout(() => {
-      const wallets = createWallets(seed as string);
+      const wallets = createAssets(seed as string);
 
       if (!wallets) {
         return;
       }
-      addWallets(wallets);
+      addAssets(wallets);
       router.push("auth/congretulation");
     }, 0);
   }, []);
 
-  return <AuthLoading label="Wallet Creating" />;
+  return <AuthLoading label="Wallet is Creating" />;
 }

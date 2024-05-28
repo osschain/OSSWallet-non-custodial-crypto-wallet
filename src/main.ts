e9a94d6e4cf42581d3f0dbe0618f89304012b8e0
amiko <amiraniburjanadze@gmail.com>
2024-05-28 03:02:37 -0700

    feat: create main.ts file

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+require('node-libs-react-native/globals');
+import "react-native-get-random-values";
+import "@ethersproject/shims";
+import "@/translations/i18n";
+
+import { Buffer } from "buffer";
+global.Buffer = Buffer;
+
+
+import "expo-router/entry";
+

8ea218a41f561f97cd0e9639969401cfc3f963e6
amiko <amiraniburjanadze@gmail.com>
2024-06-05 14:57:22 -0700

    fix:  networks fetch perfoamnce

--- src/app/api/network.ts
+++ src/app/api/network.ts
@@ -1,16 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { getNetworks } from "@/services/asset.service";
+import { useAssets } from "./assets";
 
 
 export const UseNetworks = () => {
+    const { data: assets } = useAssets()
     return useQuery({
         queryKey: ["networks"],
         queryFn: async () => {
-            const networks = getNetworks();
+            if (!assets) throw new Error("NO ASSETS")
 
-            return networks
+            const evmChains = assets.filter((asset) => asset["slip-0044"] === 60 && !asset.contractAddress)
 
+            const network = evmChains.map(chain => {
+                return { icon: chain.icon, label: chain.name }
+            })
+            return network
         },
         refetchOnWindowFocus: false,
         refetchOnMount: false

a45427b628a6f3c52bd6d5da7b36220f4077890d
amiko <amiraniburjanadze@gmail.com>
2024-06-14 04:33:09 -0700

    feat: change nft by new servicces

--- src/services/nft.service.ts
+++ src/services/nft.service.ts
@@ -1,16 +1,32 @@
-import { Blockchain } from "@ankr.com/ankr.js"
+import { Blockchain, GetNFTMetadataReply, GetNFTsByOwnerReply } from "@ankr.com/ankr.js"
 
 import { nftType } from "@/@types/nft"
-import { ankrProvider } from "@/config/ankr"
+import { ApiEndpoints, ApiResponse, httpClient } from "@/config/axios"
 
-export const getEvmNfts = async (address: string, page: number = 10) => {
+export const getEvmNfts = async (address: string, page: number = 10, pageToken: string | undefined) => {
     try {
-        const nftReference = await ankrProvider.getNFTsByOwner({
-            walletAddress: address,
-            pageSize: page
-        })
+        // const nftReference = await ankrProvider.getNFTsByOwner({
+        //     walletAddress: address,
+        //     pageSize: page
+        // })
 
-        const nft: nftType[] = nftReference.assets.map(({ tokenId, blockchain, name, symbol, collectionName, contractAddress, imageUrl }) => {
+        const response = await httpClient.post(ApiEndpoints.GET_NFT_BY_OWNER, {
+            id: 1,
+            wallet_address: address,
+            blockchain: [],
+            page_size: page,
+            page_token: pageToken,
+
+        }) as ApiResponse<GetNFTsByOwnerReply>
+
+        if (!response.data.success) {
+            throw new Error()
+        }
+
+        const nfts = response.data.ans.result
+
+
+        const nft: nftType[] = nfts.assets.map(({ tokenId, blockchain, name, symbol, collectionName, contractAddress, imageUrl }) => {
             return {
                 name,
                 symbol,
@@ -29,13 +45,21 @@ export const getEvmNfts = async (address: string, page: number = 10) => {
 }
 
 export const getEvmNft = async (contractAddress: string, blockchain: Blockchain, tokenId: string) => {
-    const nftReference = await ankrProvider.getNFTMetadata({
-        contractAddress,
+    const response = await httpClient.post(ApiEndpoints.GET_NFT_METADATA, {
+        id: 1,
+        contract_address: contractAddress,
         blockchain,
-        tokenId,
-        forceFetch: false
-    })
+        page_size: 1,
+        token_id: tokenId
+
+    }) as ApiResponse<GetNFTMetadataReply>
+
+    if (!response.data.success) {
+        throw new Error()
+    }
+
+    const nfts = response.data.ans.result
 
 
-    return nftReference.attributes
+    return nfts.attributes
 }

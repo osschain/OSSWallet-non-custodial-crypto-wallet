0e33f13fc69fe2e5cf52f7fb94923d69526b0d29
amiko <amiraniburjanadze@gmail.com>
2024-06-21 03:04:20 -0700

    fix: solve balance problem

--- src/app/api/balances.ts
+++ src/app/api/balances.ts
@@ -4,7 +4,6 @@ import { useAssets } from "./assets";
 
 import { getBalances } from "@/services/balances.service";
 
-
 export const UseBalances = () => {
     const { data: assetsManager } = useAssets()
     const assets = assetsManager?.assets;
@@ -15,10 +14,24 @@ export const UseBalances = () => {
             if (!assets) {
                 throw new Error("Asset is not presented");
             }
-            const balances = await getBalances(assetsManager.addresses);
-            return balances
+            const balances = await getBalances(assetsManager.addresses)
+
+            const shownIds = assetsManager.shownIds;
+
+            const uniqueIds = new Set();
+
+            const filteredBalances = balances.filter(balance => {
+                if (shownIds.includes(balance.id) && !uniqueIds.has(balance.id)) {
+                    uniqueIds.add(balance.id);
+                    return true;
+                }
+                return false;
+            });
+
+            return filteredBalances
         },
         refetchOnWindowFocus: false,
         refetchOnMount: false,
+        retry: true
     });
 };

46bbc87e1b1857b94f6ef125c04a148dcda06d31
amiko <amiraniburjanadze@gmail.com>
2024-06-23 01:06:28 -0700

    feat: setup dark mode at senndCConfirm modal

--- src/components/send/SendConfirm.tsx
+++ src/components/send/SendConfirm.tsx
@@ -1,10 +1,12 @@
 import { BottomSheetModal } from "@gorhom/bottom-sheet";
 import { ReactNode, forwardRef, useMemo } from "react";
 import { useTranslation } from "react-i18next";
+import { useTheme } from "styled-components/native";
 
 import ButtonUi from "../ui/ButtonUi";
 import { BodyUi, FooterUi, ScrollContainerUi } from "../ui/LayoutsUi";
 import SpacerUi from "../ui/SpacerUi";
+import { useStyledTheme } from "@/providers/StyledThemeProvider";
 
 type Props = {
   onConfirm: () => void;
@@ -15,6 +17,8 @@ type Props = {
 
 const SendConfirm = forwardRef<BottomSheetModal, Props>(
   ({ onConfirm, isLoading, children, isDetialsLoading }, ref) => {
+    const theme = useTheme();
+    const { currentMode } = useStyledTheme();
     const snapPoints = useMemo(() => ["95%", "95%"], []);
     const { t } = useTranslation();
     return (
@@ -23,6 +27,15 @@ const SendConfirm = forwardRef<BottomSheetModal, Props>(
         index={0}
         snapPoints={snapPoints}
         ref={ref}
+        handleStyle={{
+          backgroundColor: theme.colors["bg-primary"],
+        }}
+        backgroundStyle={{ backgroundColor: theme.colors["bg-primary"] }}
+        handleIndicatorStyle={{
+          backgroundColor:
+            currentMode === "dark" ? theme.colors["pure-white"] : "black",
+          borderWidth: 0,
+        }}
       >
         <ScrollContainerUi>
           <BodyUi>{children}</BodyUi>

aee43201cbddbddd57c5bcbe78243a17c137eea5
amirani <amirani>
2024-06-27 15:46:39 +0400

    feat: change eth type to ethereum

--- src/@types/balances.ts
+++ src/@types/balances.ts
@@ -1,10 +1,9 @@
-export type BalancesType = { id: string, balance: string, balanceUsd: string }
+export type BalancesType = { id: string; balance: string; balanceUsd: string };
 
 export enum AddresTypes {
-    evm = 'eth',
-    btc = 'btc',
-    solana = 'solana'
+  evm = "ethereum",
+  btc = "btc",
+  solana = "solana",
 }
 
-
-export type AddressType = { address: string, type: AddresTypes }
+export type AddressType = { address: string; type: AddresTypes };

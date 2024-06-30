c628b6578855e4b7d76072f50b08a93b1f658233
amirani <amirani>
2024-06-30 14:29:48 +0400

    feat: fox header at nft transfer

--- src/app/(wallet)/home/nft/transfer/_layout.tsx
+++ src/app/(wallet)/home/nft/transfer/_layout.tsx
@@ -5,11 +5,7 @@ import { useStackOptions } from "@/hooks/useStackOptions";
 export default function _layout() {
   const stackOptions = useStackOptions();
   return (
-    <Stack
-      screenOptions={{
-        ...stackOptions,
-      }}
-    >
+    <Stack screenOptions={stackOptions}>
       <Stack.Screen name="[slug]" options={{ title: "Transfer" }} />
     </Stack>
   );

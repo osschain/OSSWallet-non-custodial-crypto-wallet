bc1b9ddac20f41c340f233d67f8f12b2c9876892
amiko <amiraniburjanadze@gmail.com>
2024-07-01 06:28:11 -0700

    feat: create flatList Ui component

---
+++ src/components/ui/FlatListUi.tsx
@@ -0,0 +1,92 @@
+import { ComponentPropsWithoutRef } from "react";
+import { useTranslation } from "react-i18next";
+import {
+  ActivityIndicator,
+  ListRenderItem,
+  TouchableOpacity,
+} from "react-native";
+import { FlatList, RefreshControl } from "react-native-gesture-handler";
+
+import BodyTextUi from "./BodyTextUi";
+import SpacerUi from "./SpacerUi";
+
+type FlatListUiProps = {
+  data: any[];
+  renderItem: ListRenderItem<any>;
+  onRefresh: () => void;
+  pageToken?: string;
+  isRefetching?: boolean;
+  onLoadMore?: () => void;
+} & ComponentPropsWithoutRef<typeof FlatList>;
+
+const FlatListUi: React.FC<FlatListUiProps> = ({
+  data,
+  renderItem,
+  onRefresh,
+  pageToken,
+  isRefetching,
+  onLoadMore,
+}) => {
+  return (
+    <FlatList
+      data={data}
+      showsVerticalScrollIndicator={false}
+      renderItem={renderItem}
+      ListFooterComponent={() => (
+        <>
+          <LoadMore
+            nextPageToken={pageToken}
+            onLoadMore={onLoadMore}
+            isRefetching={isRefetching}
+          />
+        </>
+      )}
+      refreshControl={
+        <>
+          {onRefresh ? (
+            <RefreshControl
+              onRefresh={async () => {
+                onRefresh();
+              }}
+              refreshing={false}
+            />
+          ) : null}
+        </>
+      }
+    />
+  );
+};
+
+type LoadMoreProps = {
+  nextPageToken?: string;
+  isRefetching?: boolean;
+  onLoadMore?: () => void;
+};
+
+const LoadMore: React.FC<LoadMoreProps> = ({
+  nextPageToken,
+  isRefetching,
+  onLoadMore,
+}) => {
+  const { t } = useTranslation();
+
+  if (nextPageToken) {
+    return (
+      <SpacerUi style={{ padding: 20 }}>
+        {isRefetching ? (
+          <ActivityIndicator />
+        ) : (
+          <TouchableOpacity onPress={onLoadMore}>
+            <BodyTextUi color="blue-500" style={{ textAlign: "center" }}>
+              {t("shared.loadMore")}
+            </BodyTextUi>
+          </TouchableOpacity>
+        )}
+      </SpacerUi>
+    );
+  }
+
+  return null;
+};
+
+export default FlatListUi;

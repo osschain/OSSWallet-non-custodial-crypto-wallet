527c04f395b672d575363af76a93d28ff01d28a3
amiko <amiraniburjanadze@gmail.com>
2024-07-02 04:54:30 -0700

    update: use tokenPropertUi at custom token

--- src/app/(wallet)/home/custom-token/add-custom-token.tsx
+++ src/app/(wallet)/home/custom-token/add-custom-token.tsx
@@ -1,6 +1,5 @@
 import { Blockchain } from "@ankr.com/ankr.js";
 import { BottomSheetModal } from "@gorhom/bottom-sheet";
-import { Wallet } from "ethers";
 import { router } from "expo-router";
 import React, { useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
--- src/components/customToken/CustomTokenProps.tsx
+++ src/components/customToken/CustomTokenProps.tsx
@@ -1,8 +1,7 @@
 import { useTranslation } from "react-i18next";
 import { ActivityIndicator } from "react-native";
-import styled from "styled-components/native";
 
-import BodyTextUi from "../ui/BodyTextUi";
+import { PropertiesUi, PropertyUi } from "../ui/PropertyUi";
 import SpacerUi from "../ui/SpacerUi";
 
 import { tokenType } from "@/@types/assets";
@@ -29,53 +28,21 @@ const CustomTokenProps = ({
   }
 
   return (
-    <TokenProperties>
-      <TokenProperty
+    <PropertiesUi>
+      <PropertyUi
         label={`${t("wallet.home.custom-token.add-custom-token.name")}:`}
         value={tokenProperties.name}
       />
-      <SpacerUi size="xl">
-        <TokenProperty
-          label={`${t("wallet.home.custom-token.add-custom-token.symbol")}:`}
-          value={tokenProperties.symbol}
-        />
-      </SpacerUi>
-      <SpacerUi size="xl">
-        <TokenProperty
-          label={`${t("wallet.home.custom-token.add-custom-token.decimals")}:`}
-          value={tokenProperties.decimals.toString()}
-        />
-      </SpacerUi>
-    </TokenProperties>
+      <PropertyUi
+        label={`${t("wallet.home.custom-token.add-custom-token.symbol")}:`}
+        value={tokenProperties.symbol}
+      />
+      <PropertyUi
+        label={`${t("wallet.home.custom-token.add-custom-token.decimals")}:`}
+        value={tokenProperties.decimals.toString()}
+      />
+    </PropertiesUi>
   );
 };
-
-const TokenProperty = ({ label, value }: { label: string; value: string }) => (
-  <Row>
-    <LeftContent>
-      <BodyTextUi weight="medium">{label}</BodyTextUi>
-    </LeftContent>
-    <RightContent>
-      <BodyTextUi weight="medium" color="text-second">
-        {value}
-      </BodyTextUi>
-    </RightContent>
-  </Row>
-);
-
-const TokenProperties = styled.View`
-  padding: ${({ theme }) => theme.spaces["xl"]};
-  background-color: ${({ theme }) => theme.colors["bg-second"]};
-  border-radius: ${({ theme }) => theme.sizes["md"]};
-`;
-
-const LeftContent = styled.View``;
-
-const RightContent = styled.View``;
-
-const Row = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-`;
 
 export default CustomTokenProps;

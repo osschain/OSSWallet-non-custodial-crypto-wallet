a8e28a328509e9eb36e12fbfe0dc8e251683d03c
Amirani <amiraniburjanadze@gmail.com>
2024-07-18 18:33:41 +0400

    fix: fix some test changes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -18,6 +18,7 @@ import QueryProvider from "@/providers/QueryProvider";
 import StoreProvider from "@/providers/StoreProvider";
 import StyledThemeProvider from "@/providers/StyledThemeProvider";
 import ButtonUi from "@/components/ui/ButtonUi";
+import { usePushNotifications } from "@/hooks/usePushNotifications";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -32,62 +33,10 @@ export const unstable_settings = {
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-// --------------------------------------- ---------------------------------------- //
-
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: false,
-  }),
-});
-
 export default function RootLayout() {
-  const [expoPushToken, setExpoPushToken] = useState<string>();
-  const [notification, setNotification] = useState<
-    Notifications.Notification | undefined
-  >(undefined);
-  const responseListener = useRef<Notifications.Subscription>();
-  const notificationListener = useRef<Notifications.Subscription>();
-
-  // effect ---- efffect //
-
-  useEffect(() => {
-    // fetch expo push token
-    registerForPushNotificationsAsync().then((token) => {
-      setExpoPushToken(token);
-    });
-
-    notificationListener.current =
-      Notifications.addNotificationReceivedListener((notification) => {
-        setNotification(notification);
-        console.log("NOTIFICATON", notification);
-      });
-
-    responseListener.current =
-      Notifications.addNotificationResponseReceivedListener((response) => {
-        console.log(response, "RESPOSNE RECIEVED");
-      });
-
-    return () => {
-      notificationListener.current &&
-        Notifications.removeNotificationSubscription(
-          notificationListener.current
-        );
-
-      responseListener.current &&
-        Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
-
-  // effect ---- efffect //
-
-  console.log("TOKEN", expoPushToken);
-
-  // --------------------------------------- ---------------------------------------- //
-
   const { fontsLoaded: loaded, fontError: error } = useFont();
-
+  const { expoPushToken } = usePushNotifications();
+  console.log(expoPushToken);
   const { i18n } = useTranslation();
   useEffect(() => {
     const bootstrapAsync = async () => {
@@ -168,54 +117,4 @@ function RootLayoutNav() {
       </ErrorBoundaryProvider>
     </>
   );
-}
-
-async function registerForPushNotificationsAsync() {
-  let token;
-
-  if (Platform.OS === "android") {
-    await Notifications.setNotificationChannelAsync("default", {
-      name: "default",
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: "#FF231F7C",
-    });
-  }
-
-  if (Device.isDevice) {
-    const { status: existingStatus } =
-      await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== "granted") {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== "granted") {
-      alert("Failed to get push token for push notification!");
-      return;
-    }
-    // Learn more about projectId:
-    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
-    // EAS projectId is used here.
-    try {
-      const projectId =
-        Constants?.expoConfig?.extra?.eas?.projectId ??
-        Constants?.easConfig?.projectId;
-      if (!projectId) {
-        throw new Error("Project ID not found");
-      }
-      token = (
-        await Notifications.getExpoPushTokenAsync({
-          projectId,
-        })
-      ).data;
-      console.log(token);
-    } catch (e) {
-      token = `${e}`;
-    }
-  } else {
-    alert("Must use physical device for Push Notifications");
-  }
-
-  return token;
 }
--- src/hooks/usePushNotifications.ts
+++ src/hooks/usePushNotifications.ts
@@ -5,91 +5,91 @@ import { useState, useEffect, useRef } from "react";
 import { Platform } from "react-native";
 
 export interface PushNotificationState {
-    expoPushToken?: Notifications.ExpoPushToken;
-    notification?: Notifications.Notification;
+  expoPushToken?: Notifications.ExpoPushToken;
+  notification?: Notifications.Notification;
 }
 
 export const usePushNotifications = (): PushNotificationState => {
-    Notifications.setNotificationHandler({
-        handleNotification: async () => ({
-            shouldPlaySound: false,
-            shouldShowAlert: true,
-            shouldSetBadge: false,
-        }),
-    });
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldPlaySound: true,
+      shouldShowAlert: true,
+      shouldSetBadge: false,
+    }),
+  });
 
-    const [expoPushToken, setExpoPushToken] = useState<
-        Notifications.ExpoPushToken | undefined
-    >();
+  const [expoPushToken, setExpoPushToken] = useState<
+    Notifications.ExpoPushToken | undefined
+  >();
 
-    const [notification, setNotification] = useState<
-        Notifications.Notification | undefined
-    >();
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >();
 
-    const notificationListener = useRef<Notifications.Subscription>();
-    const responseListener = useRef<Notifications.Subscription>();
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
 
-    async function registerForPushNotificationsAsync() {
-        let token;
-        if (Device.isDevice) {
-            const { status: existingStatus } =
-                await Notifications.getPermissionsAsync();
-            let finalStatus = existingStatus;
+  async function registerForPushNotificationsAsync() {
+    let token;
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
 
-            if (existingStatus !== "granted") {
-                const { status } = await Notifications.requestPermissionsAsync();
-                finalStatus = status;
-            }
-            if (finalStatus !== "granted") {
-                alert("Failed to get push token for push notification");
-                return;
-            }
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification");
+        return;
+      }
 
-            token = await Notifications.getExpoPushTokenAsync({
-                projectId: Constants.expoConfig?.extra?.eas.projectId,
-            });
-        } else {
-            alert("Must be using a physical device for Push notifications");
-        }
-
-        if (Platform.OS === "android") {
-            Notifications.setNotificationChannelAsync("default", {
-                name: "default",
-                importance: Notifications.AndroidImportance.MAX,
-                vibrationPattern: [0, 250, 250, 250],
-                lightColor: "#FF231F7C",
-            });
-        }
-
-        return token;
+      token = await Notifications.getExpoPushTokenAsync({
+        projectId: Constants.expoConfig?.extra?.eas.projectId,
+      });
+    } else {
+      alert("Must be using a physical device for Push notifications");
     }
 
-    useEffect(() => {
-        registerForPushNotificationsAsync().then((token) => {
-            setExpoPushToken(token);
-        });
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
 
-        notificationListener.current =
-            Notifications.addNotificationReceivedListener((notification) => {
-                setNotification(notification);
-            });
+    return token;
+  }
 
-        responseListener.current =
-            Notifications.addNotificationResponseReceivedListener((response) => {
-                console.log(response);
-            });
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+      setExpoPushToken(token);
+    });
 
-        return () => {
-            Notifications.removeNotificationSubscription(
-                notificationListener.current!
-            );
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
 
-            Notifications.removeNotificationSubscription(responseListener.current!);
-        };
-    }, []);
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
 
-    return {
-        expoPushToken,
-        notification,
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current!
+      );
+
+      Notifications.removeNotificationSubscription(responseListener.current!);
     };
+  }, []);
+
+  return {
+    expoPushToken,
+    notification,
+  };
 };

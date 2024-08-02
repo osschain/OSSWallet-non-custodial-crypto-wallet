bff441ce5e83f732e46bc361d5db14370e749780
Amirani <amiraniburjanadze@gmail.com>
2024-08-02 17:25:27 +0400

    fix: show coming soon at swap

--- src/app/(wallet)/swap/[slug].tsx
+++ src/app/(wallet)/swap/[slug].tsx
@@ -9,95 +9,105 @@ import AssetOptions from "@/components/asset/AssetOptions";
 import AssetQuantityInputUi from "@/components/ui/AssetQuantityInputUi";
 import ButtonUi from "@/components/ui/ButtonUi";
 import HeaderTextUi from "@/components/ui/HeaderTextUi";
-import { BodyUi, FooterUi, ScrollContainerUi } from "@/components/ui/LayoutsUi";
+import {
+  BodyUi,
+  ContainerUi,
+  FooterUi,
+  ScrollContainerUi,
+} from "@/components/ui/LayoutsUi";
 import SpacerUi from "@/components/ui/SpacerUi";
 import { findAsset } from "@/util/findAsset";
 
 export default function Swap() {
-  const { t } = useTranslation();
-  const { slug } = useLocalSearchParams();
-  const { data: assetManager, isError } = useAssets();
-  const assets = assetManager?.assets;
-  const asset = findAsset(assets, slug as string);
+  // const { t } = useTranslation();
+  // const { slug } = useLocalSearchParams();
+  // const { data: assetManager, isError } = useAssets();
+  // const assets = assetManager?.assets;
+  // const asset = findAsset(assets, slug as string);
 
-  const fromAsseet = useRef<BottomSheetModal>(null);
-  const toAsset = useRef<BottomSheetModal>(null);
+  // const fromAsseet = useRef<BottomSheetModal>(null);
+  // const toAsset = useRef<BottomSheetModal>(null);
 
-  const [from, setFrom] = useState<AssetType | null>(asset || null);
-  const [to, setTo] = useState<AssetType | null>(null);
+  // const [from, setFrom] = useState<AssetType | null>(asset || null);
+  // const [to, setTo] = useState<AssetType | null>(null);
 
-  if (isError || !asset) {
-    // return (
-    //   <ContainerUi>
-    //     <SpacerUi>
-    //       <MessageUi> t("shared.asset-error")</MessageUi>
-    //     </SpacerUi>
-    //   </ContainerUi>
-    // );
-  }
+  // if (isError || !asset) {
+  //   // return (
+  //   //   <ContainerUi>
+  //   //     <SpacerUi>
+  //   //       <MessageUi> t("shared.asset-error")</MessageUi>
+  //   //     </SpacerUi>
+  //   //   </ContainerUi>
+  //   // );
+  // }
 
   return (
-    <ScrollContainerUi>
-      <Stack.Screen
-        options={{ title: `${t("shared.swap")} ${asset?.name || ""} ` }}
-      />
+    // <ScrollContainerUi>
+    //   <Stack.Screen
+    //     options={{ title: `${t("shared.swap")} ${asset?.name || ""} ` }}
+    //   />
 
-      {!asset && (
-        <AssetOptions
-          assets={assets as AssetType[]}
-          ref={fromAsseet}
-          onSelect={(asset) => {
-            setFrom(asset);
-            fromAsseet.current?.close();
-          }}
-        />
-      )}
+    //   {!asset && (
+    //     <AssetOptions
+    //       assets={assets as AssetType[]}
+    //       ref={fromAsseet}
+    //       onSelect={(asset) => {
+    //         setFrom(asset);
+    //         fromAsseet.current?.close();
+    //       }}
+    //     />
+    //   )}
 
-      <AssetOptions
-        assets={assets as AssetType[]}
-        ref={toAsset}
-        onSelect={(asset) => {
-          setTo(asset);
-          toAsset.current?.close();
-        }}
-      />
+    //   <AssetOptions
+    //     assets={assets as AssetType[]}
+    //     ref={toAsset}
+    //     onSelect={(asset) => {
+    //       setTo(asset);
+    //       toAsset.current?.close();
+    //     }}
+    //   />
 
-      <BodyUi>
-        <SpacerUi size="2xl">
-          <HeaderTextUi>From</HeaderTextUi>
-          <SpacerUi size="lg">
-            <AssetQuantityInputUi
-              placeholder={t("wallet.home.swap.addres-input-placeholder")}
-              uri={from?.icon}
-              title={from?.name}
-              onAssetPress={() => {
-                if (asset) return;
-                fromAsseet.current?.present();
-              }}
-            />
-          </SpacerUi>
-        </SpacerUi>
-        <SpacerUi size="2xl">
-          <HeaderTextUi>To</HeaderTextUi>
-          <SpacerUi size="lg">
-            <AssetQuantityInputUi
-              uri={to?.icon}
-              title={to?.name}
-              placeholder={t("wallet.home.swap.addres-input-placeholder")}
-              onAssetPress={() => toAsset.current?.present()}
-            />
-          </SpacerUi>
-        </SpacerUi>
+    //   <BodyUi>
+    //     <SpacerUi size="2xl">
+    //       <HeaderTextUi>From</HeaderTextUi>
+    //       <SpacerUi size="lg">
+    //         <AssetQuantityInputUi
+    //           placeholder={t("wallet.home.swap.addres-input-placeholder")}
+    //           uri={from?.icon}
+    //           title={from?.name}
+    //           onAssetPress={() => {
+    //             if (asset) return;
+    //             fromAsseet.current?.present();
+    //           }}
+    //         />
+    //       </SpacerUi>
+    //     </SpacerUi>
+    //     <SpacerUi size="2xl">
+    //       <HeaderTextUi>To</HeaderTextUi>
+    //       <SpacerUi size="lg">
+    //         <AssetQuantityInputUi
+    //           uri={to?.icon}
+    //           title={to?.name}
+    //           placeholder={t("wallet.home.swap.addres-input-placeholder")}
+    //           onAssetPress={() => toAsset.current?.present()}
+    //         />
+    //       </SpacerUi>
+    //     </SpacerUi>
+    //   </BodyUi>
+
+    //   <FooterUi marginSize="sm">
+    //     <ButtonUi
+    //       variant="primary"
+    //       onPress={() => router.replace("/(wallet)/swap/swap-in-progress")}
+    //     >
+    //       {t("shared.swap")}
+    //     </ButtonUi>
+    //   </FooterUi>
+    // </ScrollContainerUi>
+    <ContainerUi>
+      <BodyUi style={{ alignItems: "center", justifyContent: "center" }}>
+        <HeaderTextUi size="xl">Coming soon ...</HeaderTextUi>
       </BodyUi>
-
-      <FooterUi marginSize="sm">
-        <ButtonUi
-          variant="primary"
-          onPress={() => router.replace("/(wallet)/swap/swap-in-progress")}
-        >
-          {t("shared.swap")}
-        </ButtonUi>
-      </FooterUi>
-    </ScrollContainerUi>
+    </ContainerUi>
   );
 }
--- src/app/(wallet)/swap/index.tsx
+++ src/app/(wallet)/swap/index.tsx
@@ -1,14 +1,5 @@
-import BodyTextUi from "@/components/ui/BodyTextUi";
-import HeaderTextUi from "@/components/ui/HeaderTextUi";
-import { BodyUi, ContainerUi } from "@/components/ui/LayoutsUi";
+import { Redirect } from "expo-router";
 
 export default function Swap() {
-  // <Redirect href="/(wallet)/swap/slug" />;
-  return (
-    <ContainerUi>
-      <BodyUi style={{ alignItems: "center", justifyContent: "center" }}>
-        <HeaderTextUi size="xl">Coming soon ...</HeaderTextUi>
-      </BodyUi>
-    </ContainerUi>
-  );
+  return <Redirect href="/(wallet)/swap/slug" />;
 }
